628946e26dce19e44600e10e9f7f089d3a52499d
iHavebraincells <rizzbash32@gmail.com>
2025-06-20 01:50:33 -0400

    Update uv.config.js

--- src/uv.config.js
+++ src/uv.config.js
@@ -1,11 +1,6 @@
-/*global Ultraviolet*/
 self.__uv$config = {
-  prefix: "/bashW.2/service/",
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/bashW.2/uv/uv.handler.js",
-  client: "/bashW.2/uv/uv.client.js",
-  bundle: "/bashW.2/uv/uv.bundle.js",
-  config: "/bashW.2/uv/uv.config.js",
-  sw: "/bashW.2/uv/uv.sw.js",
+  prefix: '/bashW.2/uv/',
+  bare: 'https://bare.pages.dev',
+  encodeUrl: url => btoa(url),
+  decodeUrl: url => atob(url)
 };
